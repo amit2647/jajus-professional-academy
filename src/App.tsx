ece569860b22e1b9a9e5594c1f0ec135e24b2062
amit2647 <amitmahorkar798@gmail.com>
2025-10-31 17:58:36 +0530

    Deploy to Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <div className="App">
-      <Router basename="/jajus-professional-academy">
+      <Router >
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
